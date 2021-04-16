28c3805c27fa2eb08552e4e180f73c2f3e1f9aff
Erin Moon <erin@hecke.rs>
2021-04-15 23:49:45 -0500

    QuadCopter: slight cleanup

--- QuadCopter_tb.sv
+++ QuadCopter_tb.sv
@@ -1,18 +1,19 @@
+`default_nettype none
 module QuadCopter_tb();
-			
+
 //// Interconnects to DUT/support defined as type wire /////
 wire SS_n,SCLK,MOSI,MISO,INT;
 wire RX,TX;
-wire [7:0] resp;				// response from DUT
+wire [7:0] resp;                // response from DUT
 wire cmd_sent,resp_rdy;
 wire frnt_ESC, back_ESC, left_ESC, rght_ESC;
 
 ////// Stimulus is declared as type reg ///////
 reg clk, RST_n;
-reg [7:0] host_cmd;				// command host is sending to DUT
-reg [15:0] data;				// data associated with command
-reg send_cmd;					// asserted to initiate sending of command
-reg clr_resp_rdy;				// asserted to knock down resp_rdy
+reg [7:0] host_cmd;             // command host is sending to DUT
+reg [15:0] data;                // data associated with command
+reg send_cmd;                   // asserted to initiate sending of command
+reg clr_resp_rdy;               // asserted to knock down resp_rdy
 
 wire [7:0] LED;
 
@@ -20,23 +21,23 @@ wire [7:0] LED;
 
 ////////////////////////////////////////////////////////////////
 // Instantiate Physical Model of Copter with Inertial sensor //
-//////////////////////////////////////////////////////////////	
+//////////////////////////////////////////////////////////////  
 CycloneIV iQuad(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),.MISO(MISO),
                 .MOSI(MOSI),.INT(INT),.frnt_ESC(frnt_ESC),.back_ESC(back_ESC),
-				.left_ESC(left_ESC),.rght_ESC(rght_ESC));				  			
-	 
-	 
+                .left_ESC(left_ESC),.rght_ESC(rght_ESC));
+
+
 ////// Instantiate DUT ////////
 QuadCopter iDUT(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),.MOSI(MOSI),.MISO(MISO),
                 .INT(INT),.RX(RX),.TX(TX),.LED(LED),.FRNT(frnt_ESC),.BCK(back_ESC),
-				.LFT(left_ESC),.RGHT(rght_ESC));
+                .LFT(left_ESC),.RGHT(rght_ESC));
 
 
 //// Instantiate Master UART (mimics host commands) //////
 RemoteComm iREMOTE(.clk(clk), .rst_n(RST_n), .RX(TX), .TX(RX),
                      .cmd(host_cmd), .data(data), .send_cmd(send_cmd),
-					 .cmd_sent(cmd_sent), .resp_rdy(resp_rdy),
-					 .resp(resp), .clr_resp_rdy(clr_resp_rdy));
+                     .cmd_sent(cmd_sent), .resp_rdy(resp_rdy),
+                     .resp(resp), .clr_resp_rdy(clr_resp_rdy));
 
 initial begin
  
@@ -49,4 +50,4 @@ always
 
 /// perhaps include a tb_tasks file with helper tasks for testing.
 
-endmodule	
+endmodule
