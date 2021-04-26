84d254ba1d036559ce86a4079161d6921d21c665
erin marshall <erin@hecke.rs>
2021-04-26 15:06:52 -0500

    esc_interface_tb version that passes but is less elegant

--- tests/ESC_interface_tb.sv
+++ tests/ESC_interface_tb.sv
@@ -1,60 +1,89 @@
-module ESC_interface_tb();
+module ESC_interface_tb ();
   logic clk, rst_n, wrt;
-  logic PWM;
   logic [10:0] SPEED;
-  logic [15:0] high_cnt;
+  logic PWM;
 
-  ESC_interface iDUT(.clk(clk),.rst_n(rst_n),.wrt(wrt),
-            .SPEED(SPEED),.PWM(PWM));
+  ESC_interface DUT(.clk  (clk),
+                    .rst_n(rst_n),
+                    .wrt  (wrt),
+                    .SPEED(SPEED),
+                    .PWM  (PWM));
+
+  initial clk = 0;
+  always #5 assign clk = ~clk;
 
   initial begin
-    clk = 0;
     rst_n = 0;
-    wrt = 0;
+    @(posedge clk);
+    @(negedge clk) rst_n = 1;
 
-    // Case 1, SPEED is set to 0
-    SPEED = 11'h000;
-    @(negedge clk);
-    rst_n = 1;
-    repeat (2) @(posedge clk);		// wait after deasserting asynch reset
-    @(negedge clk);
+    SPEED = 0;
     wrt = 1;
-    @(negedge clk);					// wait a clk cycle
-    wrt = 0;
-    high_cnt = 16'h0000;
 
-    //@(posedge clk);					// wait a clock cycle for PWM to catch up
-
-    while(PWM !== 1'b0) begin
-      @(negedge clk) high_cnt = high_cnt + 1;
+    @(posedge clk); // one cycle to get through output flop
+    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
+    repeat (6250) begin
+      @(posedge clk);
+      if (PWM != 1) begin
+        $display("PWM pulse ended early"); $stop();
+      end
     end
-    if (high_cnt !== 13'h186a) begin // answer is currently off by 1
-      $error("ERR: PWM was not high for the correct amount of time.");
-      $error("Expected: 6250, Returned: %d",high_cnt);
+    @(posedge clk); @(posedge clk);
+    if (PWM != 0) begin
+      $display("pulse should've ended"); $stop();
     end
 
-    // Case 2, SPEED is set to half of maximum
-    SPEED = 11'h400;
-    @(negedge clk);
-    rst_n = 1;
+    SPEED = 2047;
     wrt = 1;
-    @(negedge clk);
-    wrt = 0;
-    high_cnt = 16'h0000;
 
-    //@(posedge clk);					// wait a clock cycle for PWM to catch up
-
-    while(PWM !== 1'b0) begin
-      @(negedge clk) high_cnt = high_cnt + 1;
+    @(posedge clk); // one cycle to get through output flop
+    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
+    repeat (12391) begin
+      @(posedge clk);
+      if (PWM != 1) begin
+        $display("PWM pulse ended early"); $stop();
+      end
     end
-    if (high_cnt !== 13'h246A) begin // answer is currently off by 1
-      $error("PWM was not high for the correct amount of time.");
-      $error("Expected: 9322, Returned: %d",high_cnt);
+    @(posedge clk); @(posedge clk);
+    if (PWM != 0) begin
+      $display("pulse should've ended"); $stop();
     end
 
+    SPEED = 1023;
+    wrt = 1;
+
+    @(posedge clk); // one cycle to get through output flop
+    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
+    repeat (9319) begin
+      @(posedge clk);
+      if (PWM != 1) begin
+        $display("PWM pulse ended early"); $stop();
+      end
+    end
+    @(posedge clk); @(posedge clk);
+    if (PWM != 0) begin
+      $display("pulse should've ended"); $stop();
+    end
+
+
+    SPEED = 1365;
+    wrt = 1;
+
+    @(posedge clk); // one cycle to get through output flop
+    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
+    repeat (10345) begin
+      @(posedge clk);
+      if (PWM != 1) begin
+        $display("PWM pulse ended early"); $stop();
+      end
+    end
+    @(posedge clk); @(posedge clk);
+    if (PWM != 0) begin
+      $display("pulse should've ended"); $stop();
+    end
+
+
+    $display("--> testbench passed");
     $finish();
   end
-
-  always
-    #1 clk = ~clk;
 endmodule
