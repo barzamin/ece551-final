83ab4aa643a59f81a48b76d230a549632705c93d
erin marshall <erin@hecke.rs>
2021-04-26 15:28:44 -0500

    revert to old ESC_interface

--- src/ESC_interface.sv
+++ src/ESC_interface.sv
@@ -1,37 +1,46 @@
 module ESC_interface (
   input clk,          // clock
   input rst_n,        // asynchronous reset active low
-  input wrt,          // starts an ESC pulse
+  input wrt,          // init a pulse
   input [10:0] SPEED, // speed input
-  output reg PWM      // ESC control output
+  output reg PWM      // esc control output
 );
 
-  // intermediate wire declarations
-  logic [13:0] setting;
-  logic [13:0] counter;
-  logic pulse_over;
+  // Intermediate wire declarations
+  wire [12:0] mult_rslt;
+  wire [13:0] num_ticks;
+  wire Rst, Set;
+  reg [13:0] q1;
 
-  // computes pulse length in cycles
-  assign setting = SPEED * 2'b11 + 6250;
-  // decrementing counter register to track pulse length;
-  // loaded with `setting` when `wrt` is asserted
-  always_ff @(posedge clk, negedge rst_n) begin
+  // multiply speed input by three and add to 6250 to get number of clk cycles
+  assign mult_rslt = SPEED * 2'b11;
+  assign num_ticks = mult_rslt + 13'h186a;
+
+  // First flop that counts down until num_ticks is 0
+  always @(posedge clk, negedge rst_n) begin
     if (!rst_n)
-      counter <= 0;
+      q1 <= 14'h0000;
     else if (wrt)
-      counter <= setting;
+      q1 <= num_ticks;
     else
-      counter <= counter - 1;
+      q1 <= q1 - 1'b1;
   end
 
-  // asserts when counter reaches zero
-  assign pulse_over = ~|counter;
+  // Set Rst high when q1 is all zeros
+  assign Rst = (~|(q1)) ? 1'b1 : 1'b0;
 
-  // PWM output flop; goes high on `wrt` and low on `pulse_over`
-  always_ff @(posedge clk, negedge rst_n) begin
-    if (wrt)
-      PWM <= 1;
-    else if (pulse_over || !rst_n)
-      PWM <= 0;
+  // Set goes high when wrt is high
+  assign Set = (wrt) ? 1'b1 : 1'b0;
+
+  // Final flop with asynch reset, PWM goes to 1 when set is high,
+  // 0 when Rst is high, and retains its value if neither are high.
+  always @(posedge clk, negedge rst_n) begin
+    if (!rst_n)
+      PWM <= 1'b0;
+    else if (Rst)
+      PWM <= 1'b0;
+    else if (Set)
+      PWM <= 1'b1;
   end
+
 endmodule
--- tests/ESC_interface_tb.sv
+++ tests/ESC_interface_tb.sv
@@ -1,89 +1,60 @@
-module ESC_interface_tb ();
+module ESC_interface_tb();
   logic clk, rst_n, wrt;
-  logic [10:0] SPEED;
   logic PWM;
+  logic [10:0] SPEED;
+  logic [15:0] high_cnt;
 
-  ESC_interface DUT(.clk  (clk),
-                    .rst_n(rst_n),
-                    .wrt  (wrt),
-                    .SPEED(SPEED),
-                    .PWM  (PWM));
-
-  initial clk = 0;
-  always #5 assign clk = ~clk;
+  ESC_interface iDUT(.clk(clk),.rst_n(rst_n),.wrt(wrt),
+            .SPEED(SPEED),.PWM(PWM));
 
   initial begin
+    clk = 0;
     rst_n = 0;
-    @(posedge clk);
-    @(negedge clk) rst_n = 1;
+    wrt = 0;
 
-    SPEED = 0;
+    // Case 1, SPEED is set to 0
+    SPEED = 11'h000;
+    @(negedge clk);
+    rst_n = 1;
+    repeat (2) @(posedge clk);		// wait after deasserting asynch reset
+    @(negedge clk);
     wrt = 1;
+    @(negedge clk);					// wait a clk cycle
+    wrt = 0;
+    high_cnt = 16'h0000;
 
-    @(posedge clk); // one cycle to get through output flop
-    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
-    repeat (6250) begin
-      @(posedge clk);
-      if (PWM != 1) begin
-        $display("PWM pulse ended early"); $stop();
-      end
+    //@(posedge clk);					// wait a clock cycle for PWM to catch up
+
+    while(PWM !== 1'b0) begin
+      @(negedge clk) high_cnt = high_cnt + 1;
     end
-    @(posedge clk); @(posedge clk);
-    if (PWM != 0) begin
-      $display("pulse should've ended"); $stop();
+    if (high_cnt !== 13'h186a) begin // answer is currently off by 1
+      $error("ERR: PWM was not high for the correct amount of time.");
+      $error("Expected: 6250, Returned: %d",high_cnt);
     end
 
-    SPEED = 2047;
+    // Case 2, SPEED is set to half of maximum
+    SPEED = 11'h400;
+    @(negedge clk);
+    rst_n = 1;
     wrt = 1;
+    @(negedge clk);
+    wrt = 0;
+    high_cnt = 16'h0000;
 
-    @(posedge clk); // one cycle to get through output flop
-    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
-    repeat (12391) begin
-      @(posedge clk);
-      if (PWM != 1) begin
-        $display("PWM pulse ended early"); $stop();
-      end
-    end
-    @(posedge clk); @(posedge clk);
-    if (PWM != 0) begin
-      $display("pulse should've ended"); $stop();
-    end
+    //@(posedge clk);					// wait a clock cycle for PWM to catch up
 
-    SPEED = 1023;
-    wrt = 1;
-
-    @(posedge clk); // one cycle to get through output flop
-    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
-    repeat (9319) begin
-      @(posedge clk);
-      if (PWM != 1) begin
-        $display("PWM pulse ended early"); $stop();
-      end
+    while(PWM !== 1'b0) begin
+      @(negedge clk) high_cnt = high_cnt + 1;
     end
-    @(posedge clk); @(posedge clk);
-    if (PWM != 0) begin
-      $display("pulse should've ended"); $stop();
+    if (high_cnt !== 13'h246A) begin // answer is currently off by 1
+      $error("PWM was not high for the correct amount of time.");
+      $error("Expected: 9322, Returned: %d",high_cnt);
     end
 
-
-    SPEED = 1365;
-    wrt = 1;
-
-    @(posedge clk); // one cycle to get through output flop
-    @(negedge clk); wrt = 0; // deassert wrt; it should only be asserted for one cycle
-    repeat (10345) begin
-      @(posedge clk);
-      if (PWM != 1) begin
-        $display("PWM pulse ended early"); $stop();
-      end
-    end
-    @(posedge clk); @(posedge clk);
-    if (PWM != 0) begin
-      $display("pulse should've ended"); $stop();
-    end
-
-
-    $display("--> testbench passed");
     $finish();
   end
+
+  always
+    #1 clk = ~clk;
 endmodule
